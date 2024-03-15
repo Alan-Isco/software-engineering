1ea93d7c8f1a18fc8be3fea1e4b40ab430a65500
Alan-Isco <isaacallan80@gmail.com>
2024-03-15 14:25:36 +0300

    created the server directory for the server side scripting and also configured sudo authentication routes for the project. In this I used Mongo db to store the data. Next step is to do Authorization.

--- client/src/components/postForm/PostForm.jsx
+++ client/src/components/postForm/PostForm.jsx
@@ -13,49 +13,56 @@ function PostForm({ handlePost }) {
         </div>
 
         <form onSubmit={handleSubmit}>
-          <div className={styles.formGroup}>
-            <label htmlFor="jobTitle">Job Title:</label>
-            <input type="text" id="jobTitle" name="jobTitle" required />
+          <div className={styles.left}>
+            <div className={styles.formGroup}>
+              <label htmlFor="jobTitle">Job Title:</label>
+              <input type="text" id="jobTitle" name="jobTitle" required />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="jobDescription">Job Description:</label>
+              <textarea id="jobDescription" name="jobDescription" rows={10}required />
+            </div>
+            <div className="buttons">
+            <button type="submit">Post</button>
+            <button type="close">Cancel</button>
+            </div>
+            
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="jobDescription">Job Description:</label>
-            <textarea id="jobDescription" name="jobDescription" required />
+          <div className={styles.right}>
+            <div className={styles.formGroup}>
+              <label htmlFor="jobDescription"> Choose the category:</label>
+              <select>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="jobDescription">Choose the Duration:</label>
+              <select>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="jobDescription">Choose the Experiennce:</label>
+              <select>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="jobDescription">Choose your budget:</label>
+              <select>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+            </div>
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="jobDescription"> Choose the category:</label>
-            <select>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
-            </select>
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="jobDescription">Choose the Duration:</label>
-            <select>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
-            </select>
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="jobDescription">Choose the Experiennce:</label>
-            <select>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
-            </select>
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="jobDescription">Choose your budget:</label>
-            <select>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
-            </select>
-          </div>
-
-
-          <button type="submit">Next</button>
+          
         </form>
       </div>
     </div>
